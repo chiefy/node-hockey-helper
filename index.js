f771ea9579ca07eea2131bb68193c448c5552257
Christopher 'Chief' Najewicz <chief@beefdisciple.com>
2015-10-22 21:00:11 -0400

    Refactoring w/ XO for linting. Adding in digits to replace my shitty pad module. Bumping version.

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
 #!/usr/bin/env node
 
-var
-  argv = require('minimist')(process.argv.slice(2)),
-  Hockey = require('./lib/Hockey'),
-  Promise = require('bluebird'),
-  hockey;
+var argv = require('minimist')(process.argv.slice(2));
+var _ = require('lodash');
+var hockey = require('./lib/Hockey')();
+var Promise = require('bluebird');
 
-if (!argv._ || argv._.length === 0) {
-  console.error('Please specify a path to FuckNeulionV2.jar');
-  process.exit(1);
-}
-
-hockey = new Hockey({
-  fn_jar_path: argv._[0]
-});
+_.bindAll(hockey);
 
 hockey.checkJavaAsync()
-  .then(function () {
-    console.info('Found Java!');
-  }, function () {
-    return Promise.reject('Could not find Java installed, please install Java and then try again.');
-  })
-  .then(hockey.findVLCPathAsync.bind(hockey))
-  .then(hockey.getGameIDsAsync.bind(hockey))
-  .then(hockey.pickGameAsync.bind(hockey))
-  .then(hockey.launchFNAsync.bind(hockey))
-  .then(hockey.launchVLC.bind(hockey))
-  .catch(function onCatch(err) {
-    console.error('Uh-oh! Looks like there was a problem... \n' + err);
-  });
+	.then(function foundJava() {
+		console.info('Found Java!');
+	}, function sadJava() {
+		return Promise.reject('Could not find Java installed, please install Java and then try again.');
+	})
+	.then(hockey.checkFNAsync)
+	.then(hockey.getGameIDsAsync)
+	.then(hockey.pickGameAsync)
+	.then(hockey.launchFNAsync)
+	.catch(function onCatch(err) {
+		console.error('Uh-oh! Looks like there was a problem... \n' + err);
+	});
