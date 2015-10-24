dd9ba40318ebe0d0d13b612e9169abd5e15ceb63
Christopher 'Chief' Najewicz <chief@beefdisciple.com>
2015-10-24 16:33:54 -0400

    Tweaking logging to look better using chalk, refactoring some of the check FN stuff, new safari prompt

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ var argv = require('minimist')(process.argv.slice(2));
 var _ = require('lodash');
 var hockey = require('./lib/Hockey')();
 var Promise = require('bluebird');
+var log = require('./lib/log');
 
 _.bindAll(hockey);
 
 hockey.checkJavaAsync()
 	.then(function foundJava() {
-		console.info('Found Java!');
+		log.ok('Found Java!');
 	}, function sadJava() {
 		return Promise.reject('Could not find Java installed, please install Java and then try again.');
 	})
@@ -18,5 +19,5 @@ hockey.checkJavaAsync()
 	.then(hockey.pickGameAsync)
 	.then(hockey.launchFNAsync)
 	.catch(function onCatch(err) {
-		console.error('Uh-oh! Looks like there was a problem... \n' + err);
+		log.error('Uh-oh! Looks like there was a problem... \n' + err);
 	});
